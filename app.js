992fb177c00c9483f7fee23c83355328cceec930
Xiaojin Cai <xcai0511@gmail.com>
2023-04-04 16:09:06 -0400

    a9 Mongo sp23 - fix build error

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
-mongoose.connect(CONNECTION_STRING)
+mongoose.connect(CONNECTION_STRING);
 
 const app = express()
 app.use(cors())
