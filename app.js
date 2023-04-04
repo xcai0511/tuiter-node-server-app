9ccda5f5d6f2a372a6242e537c2ede5c4507e3e7
Xiaojin Cai <xcai0511@gmail.com>
2023-04-04 15:55:05 -0400

    a9 Mongo sp23 - fix build error

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ import UserController
     from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
-const localConnectionString = 'mongodb://127.0.0.1:27017/tuiter';
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || localConnectionString;
-mongoose.connect(CONNECTION_STRING);
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+mongoose.connect(CONNECTION_STRING)
+
 const app = express()
 app.use(cors())
 app.use(express.json())
