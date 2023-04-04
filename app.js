5bb85fd7c9440610802a8404f8f52f075ad67435
Xiaojin Cai <xcai0511@gmail.com>
2023-04-04 17:13:02 -0400

    a9 Mongo sp23 - try to fix build error

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 config();
-const localConnectionString = 'mongodb://127.0.0.1:27017/tuiter';
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+const localConnectionString = 'mongodb://127.0.0.1:27017/tuiter'
+const DB_CONNECTION_STRING="mongodb+srv://xcai0511:Atlas0511?@web-dev.msjwvnj.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_STRING = DB_CONNECTION_STRING
     || localConnectionString;
 mongoose.connect(CONNECTION_STRING);
 
